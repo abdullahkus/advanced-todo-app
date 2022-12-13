a3a4fab8b89fb63a691c67aa0d8f2799293626a6
Abdullah KUŞ <abdullahkus38@hotmail.com>
2022-12-14 02:09:02 +0300

    Todo services added

---
+++ src/services/todo.service.js
@@ -0,0 +1,15 @@
+import http from '../utils/http/http'
+
+export const getAllTodo = () => http.get('/todos')
+
+export const getTodo = (todoId) => http.get(`/todos/${todoId}`)
+
+export const addTodo = (todo) => http.post('/todos/add', todo, {
+  headers: { 'Content-Type': 'application/json' },
+})
+
+export const updateTodo = (todoId, todo) => http.put(`/todos/${todoId}`, todo, {
+  headers: { 'Content-Type': 'application/json' },
+})
+
+export const deleteTodo = (todoId) => http.delete(`/todos/${todoId}`)
