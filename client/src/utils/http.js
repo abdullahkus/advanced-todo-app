2b3a994ac33878daa5f689ff37b9afd6ee4d4333
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-09-03 15:19:21 +0300

    refactor: change baseUrl in http utils

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'https://fake-backend-advanced-todo.onrender.com/',
-})
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
-export default http
+export default http;
